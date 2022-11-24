69a3730976e152f88316ff286143c82f561e4126
woongzz0110@gmail.com <woongzz0110@gmail.com>
2022-11-24 13:48:13 +0900

    Thu Nov 24 13:48:13 KST 2022: test

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# GITHUB_USERNAME="woongzz0110"
-# GITHUB_PASSWORD="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"
-# GITHUB_URL="github.com/woongzz0110/docker-sysbench.git"
-
 NOW=`date`
-
-# git remote set-url origin https://woongzz0110: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@github.com/woongzz0110/docker-sysbench.git
 
 while getopts m:t: opt
 do
