460e89d54958369f4c04cbd341721f562d8d7b6d
woongzz0110@gmail.com <woongzz0110@gmail.com>
2022-11-24 11:41:45 +0900

    -t

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+while getopts m:t: opt
+do
+    case "${opt}" in
+        m) MESSAGE=${OPTARG};;
+        t) TAG=${OPTARG};;
+    esac
+done
+
 git add .
-git commit -m $1
-git push origin master
+git commit -m $MESSAGE -t $TAG 
+git push origin master --tags
