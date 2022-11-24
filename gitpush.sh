61aac4d5e260400e377eddd6d2aabd01484ad372
woongzz0110@gmail.com <woongzz0110@gmail.com>
2022-11-24 13:35:25 +0900

    Thu Nov 24 13:35:25 KST 2022:

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-GITHUB_USERNAME="woongzz0110"
-GITHUB_PASSWORD="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"
-GITHUB_URL="github.com/woongzz0110/docker-sysbench.git"
+# GITHUB_USERNAME="woongzz0110"
+# GITHUB_PASSWORD="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"
+# GITHUB_URL="github.com/woongzz0110/docker-sysbench.git"
 
 NOW=`date`
+
+# git remote set-url origin https://woongzz0110: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@github.com/woongzz0110/docker-sysbench.git
 
 while getopts m:t: opt
 do
@@ -24,5 +26,5 @@ git commit --allow-empty-message -am "$NOW: $MESSAGE"
 echo
 
 echo "** PUSH **"
-git push https://$GITHUB_USERNAME:$GITHUB_PASSWORD@$GITHUB_URL master --follow-tags --force
+git push origin master --follow-tags --force
 echo
